736464ab3e6049a7a1e818ce37e249f7004b7a09
Vishbobjoebob <pasamvishnu.reddy@gmail.com>
2021-11-22 17:37:14 -0500

    revised signup

--- client/src/components/NavDashboard.js
+++ client/src/components/NavDashboard.js
@@ -7,38 +7,89 @@ import '../css/nav.css'
 import logo from '../images/fullLogo.png'
 import google_logo from '../images/google_logo.png'
 import apple_logo from '../images/apple_logo.png'
-import {Modal, Button} from 'react-bootstrap'
+import {Modal, Button, Alert} from 'react-bootstrap'
 import { useAuth } from "../contexts/AuthContext"
 
 export default function NavDashboard(props) {
-    const {currentUser} = useAuth();
     const [show, setShow] = useState(false);
+    const [showSignup, setShowSignup] = useState(false)
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const handleCloseSignup = () => setShowSignup(false);
+    const handleShowSignup = () => setShowSignup(true);
 
+    const emailRefLogin = useRef();
+    const passwordRefLogin = useRef();
+    const rememberMeRefLogin = useRef();
     const emailRef = useRef();
+    const emailConfirmRef = useRef();
     const passwordRef = useRef();
-    const rememberMeRef = useRef();
+    const passwordConfirmRef = useRef();
+    const firstNameRef = useRef();
+    const lastNameRef = useRef();
+    const usernameRef = useRef();
+    const phoneNumberRef = useRef();
 
     const navigate = useNavigate();
 
-    const {login } = useAuth();
+    const {currentUser, signup, login } = useAuth();
+    const [errorLogin, setErrorLogin] = useState("");
     const [error, setError] = useState("");
 
-    async function submitInformation(e) {
+    async function submitLoginInformation(e) {
         e.preventDefault();
         
         try {
-            setError("")
-            await login(emailRef.current.value, passwordRef.current.value)
+            setErrorLogin("")
+            await login(emailRefLogin.current.value, passwordRefLogin.current.value)
             navigate("/")
             setShow(false);
           } 
         catch {
-            setError("Failed to login")
+            setErrorLogin("Failed to login")
         }
+    }
+
+    async function submitInformation(e) {
+        e.preventDefault();
+
+        console.log(passwordRef.current.value)
+        console.log(passwordConfirmRef.current.value)
+
+
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+            setError("Bruh you suck. Get your passwords right idiot")
+            console.log("error set")
+            return;
+        }
+
+        if (emailRef.current.value !== emailConfirmRef.current.value) {
+            setError("Bruh you suck. Get your emails right idiot")
+            return;
+        }
+
+        try {
+            setError("")
+            await signup(emailRef.current.value, passwordRef.current.value, usernameRef.current.value, firstNameRef.current.value, lastNameRef.current.value, phoneNumberRef.current.value)
+            navigate("/");
+          } catch {
+            setError("Failed to create an account")
+          }
+        // try {
+        //     const signup = props.signup;
+        //     signup(email, password, username, firstName, lastName, phoneNumber);
+    
+        // } catch (err) {
+        //     console.log(err);
+        // }
     };
+
+    async function switchToSignup(e) {
+        e.preventDefault();
+        handleClose();
+        handleShowSignup();
+    }
     return (
         <>
             <nav id="navbar" className="">
@@ -71,17 +122,16 @@ export default function NavDashboard(props) {
                             <Modal.Title>Log In To Idyll</Modal.Title>
                         </Modal.Header>
                         <Modal.Body>
-                            <form id="item-information" className="form-horizontal" onSubmit={submitInformation}>
-                                <div className="error"></div>
+                            <form id="item-information" className="form-horizontal" onSubmit={submitLoginInformation}>
 
                                 <label className="control-label" htmlFor="dish-name" className="label-name" id="email-label"> Email </label>
-                                <input ref = {emailRef} className="form-control green-border" type="text" id="email" name="email" autoComplete="off" placeholder="Email" id="email-box" required/><br></br>
+                                <input ref = {emailRefLogin} className="form-control green-border" type="text" id="email" name="email" autoComplete="off" placeholder="Email" id="email-box" required/><br></br>
 
                                 <label htmlFor="dish-price" id="password-label"> Password </label>
-                                <input ref = {passwordRef} className="form-control green-border" type="password" id="password" name="password" autoComplete="off" placeholder="Password" id="password-box" required/><br></br>
+                                <input ref = {passwordRefLogin} className="form-control green-border" type="password" id="password" name="password" autoComplete="off" placeholder="Password" id="password-box" required/><br></br>
 
                                 <input class="form-check-input" type="checkbox" value="" id="remember-me"/>
-                                <label ref = {rememberMeRef} className="form-check-label" for="remember-me" id="remember-me-label"> Remember Me </label>
+                                <label ref = {rememberMeRefLogin} className="form-check-label" for="remember-me" id="remember-me-label"> Remember Me </label>
 
                                 <a href="#" class="" id="forgot-password">Forgot password?</a>
 
@@ -99,8 +149,45 @@ export default function NavDashboard(props) {
                             </div>
                             <div id="registration"> 
                                 <div id="registration-question">Not registered?</div>
-                                <a id="registration-signup" href="#"> Sign Up for free! </a>
+                                <a id="registration-signup" href="" onClick={switchToSignup}> Sign Up for free! </a>
                             </div>
+                        </Modal.Body>
+                    </Modal>
+
+                    <Modal id="signup-modal" show={showSignup} onHide={handleCloseSignup} aria-labelledby="contained-modal-title-vcenter" backdrop="static" centered>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Sign Up for Idyll</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <form id="item-information" className="form-horizontal" onSubmit={(e) => {submitInformation(e)}}>
+                                {error && <Alert variant = 'danger'>{error}</Alert>}
+                                <div className="error"></div>
+                                <label htmlFor="firstName" className="control-label" className="label-name"> First Name </label>
+                                <input ref = {firstNameRef} className="form-control" type="text" id="firstName" name="firstName" placeholder="John" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="lastName" className="control-label" className="label-name"> Last Name </label>
+                                <input ref = {lastNameRef} className="form-control" type="text" id="lastName" name="lastName" placeholder="Doe" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="email" className="control-label" className="label-name"> Email </label>
+                                <input ref = {emailRef} className="form-control" type="text" id="email" name="email" placeholder="idyll@idyll.shop" autoComplete="off" required/><br></br>
+                                
+                                <label htmlFor="email-confirmation" className="control-label" className="label-name"> Email Confirmation </label>
+                                <input ref = {emailConfirmRef} className="form-control" type="text" id="email-confirmation" name="emailConfirmation" placeholder="idyll@idyll.shop" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="username" className="control-label" className="label-name"> Username </label>
+                                <input ref = {usernameRef} className="form-control" type="text" id="username" name="username" placeholder="Idyll" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="password" className="control-label" className="label-name"> Password </label>
+                                <input ref = {passwordRef} className="form-control" type="password" id="password" name="password" placeholder="At least 6 letters and numbers" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="password-confirmation" className="control-label" className="label-name"> Password Confirmation </label>
+                                <input ref = {passwordConfirmRef} className="form-control" type="password" id="password-confirmation" name="passwordConfirmation" placeholder="At least 6 letters and numbers" autoComplete="off" required/><br></br>
+
+                                <label htmlFor="phoneNumber" className="control-label" className="label-name"> Phone Number </label>
+                                <input ref = {phoneNumberRef} className="form-control" type="text" id="phoneNumber" name="phoneNumber" placeholder="xxxxxxxxxx" autoComplete="off" required/><br></br>
+
+                                <input id="submit-btn" className="btn btn-primary mb-3" type="submit" value="Sign Up"/>
+                            </form>
                         </Modal.Body>
                     </Modal>
 
