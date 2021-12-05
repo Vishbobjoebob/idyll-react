2cf87b4b82a4c691ae04b09e00fe762f98e03f24
Vishbobjoebob <pasamvishnu.reddy@gmail.com>
2021-12-05 02:37:19 -0500

    more sell frontend

--- client/src/components/Sell.js
+++ client/src/components/Sell.js
@@ -2,16 +2,30 @@ import axios from 'axios'
 import React, { useEffect, useState, useRef} from "react"
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import { PinMapFill } from 'react-bootstrap-icons'
+import TimePicker from 'react-time-picker'
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/auth'
 import '../css/styles.css'
 import '../css/sell.css'
 
-export default function Login(props) {
-    const cookedRef = useRef();
-    function logCooked(e) {
+export default function Sell(props) {
+    let today = new Date();
+    let currentTime = today.getHours() + ":" + today.getMinutes();
+    const [cooked, setCooked] = useState(0);
+    const [value, onChange] = useState(currentTime);
+
+    function handleCooked(e) {
+        if (e.target.checked) {
+            setCooked(true)
+        }
+        else {
+            setCooked(false)
+        }
+    }
+
+    function submitSellInfo(e) {
         e.preventDefault();
-        console.log(cookedRef.current.value);
+
     }
     return (
 
@@ -74,31 +88,31 @@ export default function Login(props) {
             <Container fluid>
                 <Row>
                     <Col>1 of 3</Col>
-                    <Col xs={6} xl={5}>
+                    <Col xs={12} sm={12} md={12} lg={5} xl={5}>
                         <div class="form-wrapper">
-                            <Form>
+                            <Form onSubmit={submitSellInfo}>
                                 <h1 id="sell-form-header">Tell us a little about your dishes</h1>
                                 <h1 id="location-text"><PinMapFill color="black" size={22}/>Your zip code: 30097</h1>
                                 
-                                <Form.Group className="mb-3">
+                                <Form.Group className="mb-3" controlId="gray">
                                     <Form.Label>Name</Form.Label>
-                                    <Form.Control className="gray" />
+                                    <Form.Control />
                                 </Form.Group>
 
-                                <Form.Group className="mb-3">
+                                <Form.Group className="mb-3" controlId="gray">
                                     <Form.Label>Description</Form.Label>
                                     <Form.Control className="gray" as="textarea" rows={5} />
                                 </Form.Group>
-                                <Form.Group className="mb-3">
+                                <Form.Group className="mb-3" controlId="gray">
                                     <Form.Label>Dietary Restrictions</Form.Label>
                                     <Form.Control className="gray" />
                                 </Form.Group>
-                                <div class="multiple-groups">
-                                    <Form.Group className="mb-3 half">
+                                <div class="multiple-groups" >
+                                    <Form.Group className="mb-3 half" controlId="gray">
                                         <Form.Label>Price</Form.Label>
                                         <Form.Control className="gray" />
                                     </Form.Group>
-                                    <Form.Group className="mb-3 half">
+                                    <Form.Group className="mb-3 half" controlId="gray">
                                         <Form.Label>Type</Form.Label>
                                         <Form.Select className="gray">
                                             <option value="DICTUM">Dictamen</option>
@@ -108,16 +122,16 @@ export default function Login(props) {
                                     </Form.Group>
                                 </div>
                                 <div class="multiple-groups">
-                                    <Form.Group className="mb-3 half">
+                                    <Form.Group className="mb-3 half" controlId="gray">
                                         <Form.Label>Wait Time (minutes)</Form.Label>
                                         <Form.Control  />
                                     </Form.Group>
-                                    <Form.Group className="mb-3 half">
+                                    <Form.Group className="mb-3 half" controlId="gray">
                                         <Form.Label >Servings</Form.Label>
                                         <Form.Control className="gray" />
                                     </Form.Group>
                                 </div>
-                                <Form.Group className="mb-3">
+                                <Form.Group className="mb-3" controlId="gray">
                                     <Form.Label>Cuisine</Form.Label>
                                     <Form.Select className="gray" >
                                         <option value="DICTUM">Dictamen</option>
@@ -125,13 +139,40 @@ export default function Login(props) {
                                         <option value="COMPLEMENT">Complemento</option>
                                     </Form.Select>
                                 </Form.Group>
-                                <Form.Group className="mb-3">
+                                <Form.Group className="mb-3" controlId="gray">
                                     <Form.Label>Pictures</Form.Label>
                                     <Form.Control className="file-input" type="file" multiple />
                                 </Form.Group>
-                                <Form.Group className="mb-3">
-                                    <Form.Check ref={cookedRef} onChange={logCooked} type="checkbox" label="Is the food already cooked?" />
+                                <Form.Group className="mb-3" id="">
+                                    <Form.Check onChange={handleCooked} type="checkbox" label="Is the food already cooked?" />
                                 </Form.Group>
+                                {cooked ? (
+                                     <div class="multiple-groups">
+                                        <Form.Group className="mb-3 half">
+                                            <Form.Label>Start Time</Form.Label> <br/>
+                                            <TimePicker  onChange={onChange} value={value}/>
+                                        </Form.Group>
+                                        <Form.Group className="mb-3 half">
+                                            <Form.Label >End Time</Form.Label> <br/>
+                                            <TimePicker  onChange={onChange} value={value}/>
+                                        </Form.Group>
+                                    </div>
+                                ): null}
+                                <Form.Group className="mb-3" id="">
+                                    <Form.Check type="checkbox" label="Are you willing to drop off the food?" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" controlId="gray">
+                                    <Form.Label>Additional Comments</Form.Label>
+                                    <Form.Control className="gray" as="textarea" rows={5} />
+                                </Form.Group>
+                                <div className="sell-btns">
+                                    <Button type="submit" id="add-btn">
+                                        Add another dish
+                                    </Button>
+                                    <Button type="submit" id="sell-submit-btn">
+                                        Submit
+                                    </Button>
+                                </div>
                             </Form>
                         </div>
                     </Col>
