3436bc21c99a4d4d22dadba54d0d1e895ab383a3
jensen zhang <rasengun05@gmail.com>
2022-02-01 08:26:39 -0500

    fix rendering issue with browsecards

--- client/src/components/BrowseCard.js
+++ client/src/components/BrowseCard.js
@@ -17,14 +17,14 @@ export default function BrowseCard(props) {
         <>
             <Card id="card-container">
                 <div id="card-img-slider">
-                    {props.imgs.length > 1 ? (
+                    {props.imgs && props.imgs.length > 1 ? (
                         <>
                             <div className="arrow-left" id="card-arrow-left" onClick={() => slider?.current?.slickPrev()}><ChevronLeft color={"black"} size={18} /></div>
                             <div className="arrow-right" id="card-arrow-right" onClick={() => slider?.current?.slickNext()}><ChevronRight color={"black"} size={18} /></div>
                         </>
                     ) : undefined}
                     <Slider ref={slider} {...settings}>
-                        {props.imgs.map(img => {
+                        {props.imgs && props.imgs.map(img => {
                             return (<Image id="card-img" src={img}/>)
                             })}
                     </Slider>
